b2d7aadac3cc9c93101481a7c467ce85fdd74aad
sreeise <reeisesean@gmail.com>
2019-09-09 01:54:55 -0400

    Add derives for serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 pub extern crate derive_from_as;
 pub extern crate from_as_file;
+pub extern crate serde;
+pub extern crate serde_derive;
+pub extern crate serde_yaml;
+pub extern crate serde_json;
+pub extern crate toml;
 
 pub use derive_from_as::FromFile;
 pub use derive_from_as::AsFile;
